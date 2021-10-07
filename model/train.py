0b383fbcb7c618ee6a9c8335117cdd7ea9c8f72c
king <chenxie86@gmail.com>
2021-10-07 19:17:07 +0800

    ✨Notify the training progress to gitter

--- model/train.py
+++ model/train.py
@@ -1,8 +1,11 @@
 import logging
 import os
 import sys
+import time
 
+from dotenv import load_dotenv
 import gflags
+import notifiers
 import tensorflow as tf
 from tensorflow import keras
 
@@ -12,6 +15,7 @@ from model import make_or_restore_model
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
+NUM_EPOCHS = 100
 EVALUATE_SIZE = 1280
 
 Flags = gflags.FLAGS
@@ -20,6 +24,21 @@ gflags.DEFINE_string("ckpt_path", "./ckpt", "checkpoint dir path")
 gflags.DEFINE_bool("train", True, "train or test")
 
 TRAIN_BATCH = 16
+
+class GitterNotify(keras.callbacks.Callback):
+    def __init__(self):
+        self.gitter = notifiers.get_notifier('gitter')
+        dotenv = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".env")
+        load_dotenv(dotenv)
+        self.token = os.getenv("GITTER_TOKEN")
+        self.room_id = os.getenv("ROOM_ID")
+        if self.token is None or self.room_id is None:
+            logger.error("gitter token and room id is None")
+        self.message = "Epoch: {}/{}\nLoss: {}\nAccuracy: {}"
+    def on_epoch_end(self, epoch, logs=None):
+        msg = self.message.format(epoch+1, NUM_EPOCHS, round(logs['loss'], 3), round(logs['sparse_categorical_accuracy']*100, 3))
+        print(self.token, self.room_id, msg)
+        self.gitter.notify(token=self.token, room_id=self.room_id, message=msg)
 
 def main(argv):
     try:
@@ -42,8 +61,10 @@ def main(argv):
         sys.exit(-1)
     if Flags.train:
         callbacks = [
+                GitterNotify(),
                 keras.callbacks.ModelCheckpoint(
-                    filepath=ckpt_dir + "/ckpt-loss={loss:.2f}", save_freq=100
+                    filepath=ckpt_dir + "/ckpt.{epoch:02d}-{loss:.4f}-{sparse_categorical_accuracy:.4f}",
+                    save_freq=1000
                     ),
                 keras.callbacks.TensorBoard(
                     log_dir="./logs", histogram_freq=1, update_freq="batch"
@@ -51,7 +72,7 @@ def main(argv):
                 ]
         logger.info("========train========")
         train_dataset = full_dataset
-        model.fit(train_dataset, batch_size=TRAIN_BATCH, epochs=100,
+        model.fit(train_dataset, batch_size=TRAIN_BATCH, epochs=NUM_EPOCHS,
                 steps_per_epoch=full_dataset_size/TRAIN_BATCH, callbacks=callbacks)
     else:
         logger.info("========evaluate========")
